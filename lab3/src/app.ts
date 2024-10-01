84cc8dd1f34d89d5089de93c792a4433ebd5fe4f
scrfc <grdevg2.0gmail.com>
2024-10-01 10:14:48 +0300

    1101015

--- lab3/src/app.ts
+++ lab3/src/app.ts
@@ -322,8 +322,14 @@ class App {
 
     const bookId = parseInt(bookIdInput.value);
     const userId = parseInt(userIdInput.value);
+  
 
-    const book = this.bookLibrary.findById(
+
+
+
+
+
+    let book = this.bookLibrary.findById(
       bookId,
       (b: Book) => b.id === bookId
     );
